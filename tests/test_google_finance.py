1af57c21531c965741ff0c6f27807961e6623342
Abdul Ansari <76679241+Abdul24-code@users.noreply.github.com>
2024-09-13 20:11:04 +0530

    Update test_google_finance.py

--- tests/test_google_finance.py
+++ tests/test_google_finance.py
@@ -9,7 +9,6 @@ class TestGoogleFinance(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        # Set Chrome options for headless mode
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
@@ -17,20 +16,16 @@ class TestGoogleFinance(unittest.TestCase):
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--remote-debugging-port=9222")
 
-        # Initialize Chrome WebDriver
         cls.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         cls.google_finance_page = GoogleFinancePage(cls.driver)
 
     def test_google_finance_stocks(self):
-        # Load the Google Finance page
         print("Loading Google Finance page...")
         self.google_finance_page.load()
 
-        # Verify the page title
         print("Verifying the page title...")
         self.assertIn("Google Finance", self.google_finance_page.get_page_title())
 
-        # Retrieve stock symbols with error handling
         print("Retrieving stock symbols...")
         try:
             retrieved_symbols = self.google_finance_page.get_stock_symbols()
@@ -39,10 +34,8 @@ class TestGoogleFinance(unittest.TestCase):
             print("Error while retrieving stock symbols:", e)
             raise e
 
-        # Test data to compare against
         given_test_data = ["NFLX", "MSFT", "TSLA"]
 
-        # Find symbols in UI not in given data
         symbols_in_ui_not_in_data = set(retrieved_symbols) - set(given_test_data)
         symbols_in_data_not_in_ui = set(given_test_data) - set(retrieved_symbols)
 
@@ -51,7 +44,6 @@ class TestGoogleFinance(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        # Close the browser
         cls.driver.quit()
 
 if __name__ == "__main__":
